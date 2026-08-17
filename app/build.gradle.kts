3d225b4608ece4ddc186cd4710de32f72ba0952b
Johnmarc Jayme <johnmarcjayme1052006@gmail.com>
2026-08-17 20:40:10 +0800

    Bump version to 0.27.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
     defaultConfig {
         applicationId = "app.kizuna"
 
-        versionCode = 33
-        versionName = "0.26.0"
+        versionCode = 34
+        versionName = "0.27.0"
 
         buildConfigField("String", "COMMIT_COUNT", "\"${getLatestCommitCount()}\"")
         buildConfigField("String", "COMMIT_SHA", "\"${getLatestCommitSha()}\"")
